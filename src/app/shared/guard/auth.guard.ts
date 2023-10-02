af3a0431b14c2be91c2139fe5548dfa65cc1fd7a
Stefan Loncaric <stefan.loncaric@consulteer.com>
2023-10-02 13:18:54 +0200

    [Fixed] deps

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(public authService: AuthService, public router: Router) {}
 
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
